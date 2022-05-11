64e946808cad986d2908b668344d28ece76dee50
jimmyforsstrom <jimmyforsstrom>
2022-05-11 13:21:23 +0200

    lll

--- myzoo/src/components/Animal.tsx
+++ myzoo/src/components/Animal.tsx
@@ -1,4 +1,3 @@
-import { isDisabled } from "@testing-library/user-event/dist/utils";
 import {useEffect, useState } from "react"
 import {useParams } from "react-router-dom";
 import { IExtendedAnimal } from "../models/IExtendedAnimal";
@@ -29,7 +28,7 @@ export const Animal = () => {
 //            timeLeft();
             
             let animalsFromLS = JSON.parse(localStorage.getItem('myanimals') || '[]');
-            animalsFromLS.forEach((animals : IExtendedAnimal) => {
+            animalsFromLS.foreach((animals : IExtendedAnimal) => {
                 if(params.id == animals.id){
                     setExAnimal(animals);
                     animals.isFed = true;
@@ -38,15 +37,15 @@ export const Animal = () => {
                        
                     localStorage.setItem('myanimals', JSON.stringify(animalsFromLS));
                 
-                }});
-        }  
-    return <>
+                }};
+            }  
+    return (<>
     <h2>{extendedAnimal?.name}</h2>
     {/* <img src={extendedAnimal?.imageUrl} alt='pix'/> */}
         <div>{extendedAnimal?.shortDescription}</div>
     <h4>matad senast = {extendedAnimal?.lastFed}</h4>
     <button onClick={handleClick} disabled ={buttonDisable}>Mata mig</button>
-    </> ;
+    </> );
 
     }
     
